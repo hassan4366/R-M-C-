b80aefd51e1e3240a189932e736d6b04ab85a86c
Hassan Hammam <hammamhassan562@gmail.com>
2025-06-30 00:11:09 +0300

    Add files via upload

---
+++ C++/assin1-8.cpp
@@ -0,0 +1,53 @@
+//assin-1
+#include <iostream>
+
+int main()
+{
+  std::cout << "Line One\n";
+  std::cout << "Line Two\n";
+  std::cout << "Line Three\n";
+  return 0;
+}
+//assin-2
+/*
+  iostream Header File Content
+  -- cout
+  -- cin
+  -- cerr
+  -- istream
+  -- clog
+*/
+//assin-3
+#include <iostream>
+
+int main() 
+{
+   std::cout <<"Iam Learning Programming\nWithElzero\nWebSchool\n<3";
+   return 0;
+}
+//assin-4
+#include <iostream>
+
+int main()
+{
+   std::cout<<"|=======================|\n";
+   std::cout<<"|== Elzero Web School ==|\n";
+   std::cout<<"|==        C++        ==|\n";
+   std::cout<<"|=======================|";
+   return 0;
+}
+//assin-5
+
+#include <iostream> /* Preprocessor Directive */
+
+int main()
+{
+  std::cout  << "Line One\n";
+  std::cout << "Line Two\n"; /* cout Function */
+  std::cout << "Line Three\n";
+  // Return 0
+  return 0;
+} // End Main Function
+
+//solve the problem on day-1 (solve 6 problems-Done)
+
